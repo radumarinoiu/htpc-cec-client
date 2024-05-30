7d4f72ad98c62fdbd25060bfa3f87d0881dd1feb
Radu Florin Marinoiu <radumarinoiu98@gmail.com>
2024-05-30 21:34:45 +0300

    Reconfigured logging

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import logging
 
+from htpc_cec_client.remote_logger import RemoteHandler
 from power_management import WindowsPowerManagement
 
 
 if __name__ == '__main__':
     logger = logging.getLogger()
-    logging.basicConfig(filename='client.log', level=logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    logger.addHandler(logging.FileHandler("client.log"))
+    logger.addHandler(RemoteHandler())
     WindowsPowerManagement().listen()
